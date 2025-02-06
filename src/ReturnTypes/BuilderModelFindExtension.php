b394eba5805727423071fac9b53ea50dd7e920f4
Can Vural <can9119@gmail.com>
2025-02-06 22:03:36 +0100

    fix CS

--- src/ReturnTypes/BuilderModelFindExtension.php
+++ src/ReturnTypes/BuilderModelFindExtension.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\Str;
-use Larastan\Larastan\Methods\ModelTypeHelper;
 use Larastan\Larastan\Support\CollectionHelper;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
